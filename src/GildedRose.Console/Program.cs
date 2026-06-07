ae7929f5fdde7e5895abfbd7e28fa5e7bd32ee4a
anishpal89 <anishpal.2020@gmail.com>
2026-06-07 13:13:14 +0530

    git commit -m "Added support for conjured items"

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System;
 
 namespace GildedRose.Console;
 
@@ -9,26 +10,24 @@ public class Program
     static void Main(string[] args)
     {
         System.Console.WriteLine("OMGHAI!");
-        //test github commit
 
         var app = new Program()
-                      {
-                          Items = new List<Item>
-                                      {
-                                          new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                          new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                          new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                          new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                          new Item
-                                              {
-                                                  Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                  SellIn = 15,
-                                                  Quality = 20
-                                              },
-                                          new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                      }
-
-                      };
+        {
+            Items = new List<Item>
+            {
+                new Item { Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20 },
+                new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 },
+                new Item { Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 },
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+            }
+        };
 
         app.UpdateQuality();
 
@@ -39,72 +38,79 @@ public class Program
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            var item = Items[i];
+
+            if (item.Name != "Aged Brie" &&
+                item.Name != "Backstage passes to a TAFKAL80ETC concert")
             {
-                if (Items[i].Quality > 0)
+                if (item.Quality > 0)
                 {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    if (item.Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        Items[i].Quality = Items[i].Quality - 1;
+                        var degradeAmount = item.Name.StartsWith("Conjured") ? 2 : 1;
+
+                        item.Quality = Math.Max(0, item.Quality - degradeAmount);
                     }
                 }
             }
             else
             {
-                if (Items[i].Quality < 50)
+                if (item.Quality < 50)
                 {
-                    Items[i].Quality = Items[i].Quality + 1;
+                    item.Quality = item.Quality + 1;
 
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].SellIn < 11)
+                        if (item.SellIn < 11)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
 
-                        if (Items[i].SellIn < 6)
+                        if (item.SellIn < 6)
                         {
-                            if (Items[i].Quality < 50)
+                            if (item.Quality < 50)
                             {
-                                Items[i].Quality = Items[i].Quality + 1;
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }
                 }
             }
 
-            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
             {
-                Items[i].SellIn = Items[i].SellIn - 1;
+                item.SellIn = item.SellIn - 1;
             }
 
-            if (Items[i].SellIn < 0)
+            if (item.SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
+                if (item.Name != "Aged Brie")
                 {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        if (Items[i].Quality > 0)
+                        if (item.Quality > 0)
                         {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            if (item.Name != "Sulfuras, Hand of Ragnaros")
                             {
-                                Items[i].Quality = Items[i].Quality - 1;
+                                var degradeAmount = item.Name.StartsWith("Conjured") ? 2 : 1;
+
+                                item.Quality = Math.Max(0, item.Quality - degradeAmount);
                             }
                         }
                     }
                     else
                     {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                        item.Quality = 0;
                     }
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (item.Quality < 50)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,12 +1,95 @@
 using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
 
 namespace GildedRose.Tests;
 
 public class TestAssemblyTests
 {
     [Fact]
-    public void TestTheTruth()
+    public void Normal_Item_Degrades_By_One()
     {
-        Assert.True(true);
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Normal Item",
+                    SellIn = 10,
+                    Quality = 20
+                }
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(19, app.Items[0].Quality);
+        Assert.Equal(9, app.Items[0].SellIn);
+    }
+
+    [Fact]
+    public void Aged_Brie_Increases_In_Quality()
+    {
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Aged Brie",
+                    SellIn = 2,
+                    Quality = 0
+                }
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(1, app.Items[0].Quality);
+    }
+
+    [Fact]
+    public void Sulfuras_Never_Changes()
+    {
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = 0,
+                    Quality = 80
+                }
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(80, app.Items[0].Quality);
+        Assert.Equal(0, app.Items[0].SellIn);
+    }
+
+    [Fact]
+    public void Conjured_Items_Degrade_Twice_As_Fast()
+    {
+        var app = new Program
+        {
+            Items = new List<Item>
+            {
+                new Item
+                {
+                    Name = "Conjured Mana Cake",
+                    SellIn = 3,
+                    Quality = 6
+                }
+            }
+        };
+
+        app.UpdateQuality();
+
+        Assert.Equal(4, app.Items[0].Quality);
+        Assert.Equal(2, app.Items[0].SellIn);
     }
 }
